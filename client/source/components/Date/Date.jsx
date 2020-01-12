e7e73f3c67044500226c0d968fb1fa464bfeba24
mohsohail <sohail.momin1@gmail.com>
2020-01-12 23:07:14 +0530

    onClick handler for date selection

--- client/source/components/Date/Date.jsx
+++ client/source/components/Date/Date.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
 
+import '../../../style.scss';
 import './Date.scss';
 
+const handleClick = e => {
+  e.target.style.color = '#ffcf40';
+};
 const Date = ({ year, day, monthIndex, getDaysFromMonths }) => {
   return (
     <div className="dates">
       {getDaysFromMonths(year, monthIndex, day).map((day, dayIndex) => {
         return (
-          <p key={dayIndex} className="date">
+          <p key={dayIndex} className="date" onClick={handleClick}>
             {day}
           </p>
         );
